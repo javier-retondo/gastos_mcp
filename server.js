89f07e342d9a30f08c3e1bb9cd07ce1da2da4cfd
Javier Retondo <jeretondo@teco.com.ar>
2026-02-05 18:18:40 -0300

    fix

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const bcrypt = require('bcryptjs');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
+// Behind reverse proxy
+app.set('trust proxy', true);
 
 app.use(express.json({ limit: '256kb' }));
 app.use(express.urlencoded({ extended: true, limit: '256kb' }));
